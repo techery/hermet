83480fc38a25e6c1189f78fd7dcd21357edd2561
slava.p <slava.p@techery.io>
2016-11-23 13:26:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# techery-proxy-service
+Techery internal service for proxying third party services and stub them if needed.
+ 
+### System requirements
+ 
+ * Nodejs ~4.1
+ * npm    ~3.8.6
+ 
+
+### Installation.
+
+#### Use npm and install all local dependencies
+
+```
+npm install
+```
+
+This command will install all the needed npm dependencies.
+
+#### Set required environment variables
+
+```
+cp .env.sample .env
+```
+Then set custom variables as needed
+
+### Running service
+
+```
+npm start
+```
+
+This command invokes `node bin/server.js` command which is responsible for starting the service.
---
+++ src/app.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+
+let index = require('./routes/index');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status( 500);
+  res.json('Error: ' + err.message);
+});
+
+module.exports = app;
