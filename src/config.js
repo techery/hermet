c6e0ac3cbf015d7f0f1e2931e243665d7c27e7c0
slava.p <slava.p@techery.io>
2016-12-15 13:47:48 +0200

    Change base hermet url for api part of service

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const config = {
   app: {
     hermet_api_port: _getEnv('HERMET_API_PORT', 5000),
     hermet_proxy_port: _getEnv('HERMET_PROXY_PORT', 5050),
-    hermet_api_base_url: _getEnv('HERMET_API_BASE_URL', 'http://localhost:5000'),
+    hermet_api_base_url: _getEnv('HERMET_API_BASE_URL', 'http://localhost:5000/api'),
     hermet_session_header: _getEnv('HERMET_SESSION_HEADER', 'x-session-id')
   },
   log: {
