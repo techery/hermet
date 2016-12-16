7b008035556cee74fb2e22555fe14a0ea89abb4e
Artem Volosevich <kaminooni.box@gmail.com>
2016-12-16 12:05:32 +0000

    Fix logger errors (eslint)

--- src/components/logger.js
+++ src/components/logger.js
@@ -1,11 +1,16 @@
-'use strict';
-
 const config = require('../config');
-let curlify = require('request-as-curl'),
-  winston = require('winston'),
-  path = require('path');
+let curlify = require('request-as-curl');
+let path = require('path');
+let winston = require('winston');
+
 require('winston-daily-rotate-file');
 
+/**
+ * Setup logger
+ *
+ * @param {string} logFileName
+ * @returns {winston.Logger}
+ */
 function initLogger(logFileName) {
   let transport = new winston.transports.DailyRotateFile({
     filename: path.join('log/', logFileName),
@@ -20,26 +25,52 @@ function initLogger(logFileName) {
       new (winston.transports.Console)(),
       transport
     ]
-  })
+  });
 }
 
 class Logger {
+  /**
+   * @param {winston.Logger} loggerInstance
+   */
   constructor(loggerInstance) {
     this.loggerInstance = loggerInstance;
   }
 
+  /**
+   * Log info message
+   *
+   * @param {string} message
+   * @param {*} data
+   */
   info(message, data) {
     this.loggerInstance.log('info', message, data);
   }
 
+  /**
+   * Log debug message
+   *
+   * @param {string} message
+   * @param {*} data
+   */
   debug(message, data) {
     this.loggerInstance.log('debug', message, data);
   }
 
+  /**
+   * Log error message
+   *
+   * @param {string} message
+   * @param {*} data
+   */
   error(message, data) {
     this.loggerInstance.log('error', message, data);
   }
 
+  /**
+   * @param {Object} req
+   * @param {*} data
+   * @returns {*}
+   */
   curlify(req, data) {
     return curlify(req, data);
   }
