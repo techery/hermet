d09a8b6d1be4f394b4fd297a9eb61c1a89ea149f
Artem Volosevich <kaminooni.box@gmail.com>
2016-12-16 11:21:53 +0000

    Add config to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,27 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 
-gulp.task('build', function () {
-  return gulp.src('src/**/*.js')
+const config = {
+  dirs: {
+    src: './src/**/*.js',
+    build: './dist'
+  },
+  eslint: {
+    formatter: 'stylish'
+  }
+};
+
+gulp.task('build', () => {
+  return gulp.src(config.dirs.src)
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(config.dirs.build));
 });
 
 gulp.task('lint', () => {
-  return gulp.src('src/**/*.js')
+  return gulp.src(config.dirs.src)
     .pipe(eslint())
-    .pipe(eslint.format('stylish'))
+    .pipe(eslint.format(config.eslint.formatter))
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['build', 'lint']);
+gulp.task('default', ['lint', 'build']);
