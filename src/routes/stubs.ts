7af0a9a2567441f970c0e133330a6b3eb4aa0ac8
slava.p <slava.p@techery.io>
2016-12-23 11:23:37 +0200

    Modify Stubs routes with Session request

--- src/routes/stubs.ts
+++ src/routes/stubs.ts
@@ -1,17 +1,18 @@
 import {Router} from 'express';
 import wrap from './wrapper';
 import {stubsController} from '../container';
+import {SessionRequest} from '../interfaces/requests/SessionRequest';
 
 let router = Router();
 
 router.route('/:serviceId/stubs')
-  .get(wrap(async (req, res, next) => stubsController.list(req, res)))
-  .post(wrap(async (req, res, next) => stubsController.create(req, res)))
-  .delete(wrap(async (req, res, next) => stubsController.removeAll(req, res)));
+  .get(wrap(async (req: SessionRequest, res, next) => stubsController.list(req, res)))
+  .post(wrap(async (req: SessionRequest, res, next) => stubsController.create(req, res)))
+  .delete(wrap(async (req: SessionRequest, res, next) => stubsController.removeAll(req, res)));
 
 router.route('/:serviceId/stubs/:stubId')
-  .get(wrap(async (req, res, next) => stubsController.get(req, res)))
-  .put(wrap(async (req, res, next) => stubsController.update(req, res)))
-  .delete(wrap(async (req, res, next) => stubsController.remove(req, res)));
+  .get(wrap(async (req: SessionRequest, res, next) => stubsController.get(req, res)))
+  .put(wrap(async (req: SessionRequest, res, next) => stubsController.update(req, res)))
+  .delete(wrap(async (req: SessionRequest, res, next) => stubsController.remove(req, res)));
 
 export default router;
