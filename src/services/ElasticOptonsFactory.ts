efd8b822b938938c9cdf97f872f2ad4582ed047e
slava.p <slava.p@techery.io>
2016-12-23 11:25:22 +0200

    Refactor repositories and Elastic interaction

---
+++ src/services/ElasticOptonsFactory.ts
@@ -0,0 +1,138 @@
+import IndexDocumentParams = Elasticsearch.IndexDocumentParams;
+import GetParams = Elasticsearch.GetParams;
+import UpdateDocumentParams = Elasticsearch.UpdateDocumentParams;
+import DeleteDocumentParams = Elasticsearch.DeleteDocumentParams;
+import SearchParams = Elasticsearch.SearchParams;
+import DeleteDocumentByQueryParams = Elasticsearch.DeleteDocumentByQueryParams;
+
+export default class ElasticOptionsFactory {
+
+    protected index: string;
+
+    /**
+     * @param {string} index
+     */
+    constructor(index: string) {
+        this.index = index;
+    }
+
+    /**
+     * @param {string} type
+     * @param {Object} data
+     * @param {string} parentId
+     * @param {string} ttl
+     *
+     * @returns {IndexDocumentParams<any>}
+     */
+    public getIndexParams(type: string, data: any, parentId: string, ttl: string): IndexDocumentParams<any> {
+        let options: IndexDocumentParams<any> = {
+                index: this.index,
+                type: type,
+                body: data,
+                refresh: 'true'
+            } as IndexDocumentParams<any>;
+
+        if (parentId) {
+            options.parent = parentId;
+        }
+
+        if (ttl) {
+            options.ttl = ttl;
+        }
+
+        return options;
+    }
+
+    /**
+     * @param {string} type
+     * @param {string} id
+     * @param {string} parentId
+     *
+     * @returns {GetParams}
+     */
+    public getGetParams(type: string, id: string, parentId: string): GetParams {
+        let options: GetParams = {
+                index: this.index,
+                type: type,
+                id: id
+            };
+
+        if (parentId) {
+            options.parent = parentId;
+        }
+
+        return options;
+    }
+
+    /**
+     * @param {string} type
+     * @param {string} id
+     * @param {Object} data
+     * @param {string} parentId
+     *
+     * @returns {UpdateDocumentParams}
+     */
+    public getUpdateParams(type: string, id: string, data: any, parentId: string): UpdateDocumentParams {
+        let options: UpdateDocumentParams = {
+            index: this.index,
+            type: type,
+            id: id,
+            refresh: true,
+            body: {
+                doc: data
+            }
+        };
+
+        if (parentId) {
+            options.parent = parentId;
+        }
+
+        return options;
+    }
+
+    /**
+     * @param {string} type
+     * @param {string} id
+     * @param {string} parentId
+     *
+     * @returns {DeleteDocumentParams}
+     */
+    public getDeleteParams(type: string, id: string, parentId: string): DeleteDocumentParams {
+        let options: DeleteDocumentParams = {
+            index: this.index,
+            type: type,
+            id: id
+        };
+
+        if (parentId) {
+            options.parent = parentId;
+        }
+
+        return options;
+    }
+
+    /**
+     * @param {string} type
+     *
+     * @returns {SearchParams}
+     */
+    public getSearchParams(type: string): SearchParams {
+        return {
+            index: this.index,
+            type: type
+        } as SearchParams;
+    }
+
+    /**
+     * @param {string} type
+     *
+     * @returns {any}
+     */
+    public getDeleteByQueryParams(type: string): any {
+        return {
+            index: this.index,
+            type: type,
+            conflicts: 'proceed'
+        };
+    }
+}
--- src/services/ElasticWrapper.ts
+++ src/services/ElasticWrapper.ts
@@ -9,19 +9,17 @@ import {
 
 export default class ElasticWrapper {
     protected client: Client;
-    protected index: string;
 
     /**
      * @param {Client} client
-     * @param {string} index
      */
-    constructor(client: Client, index: string) {
+    constructor(client: Client) {
         this.client = client;
-        this.index = index;
     }
 
     /**
      * @param {*} body
+     *
      * @returns {Promise}
      */
     protected promisify(body: any): Promise<any> {
@@ -36,132 +34,65 @@ export default class ElasticWrapper {
     }
 
     /**
-     * @param {string} type
-     * @param {Object} data
-     * @param {string} parentId
+     * @param {IndexDocumentParams<any>} options
+     *
      * @returns {Promise}
      */
-    public create(type: string, data: any, parentId?: string): Promise<any> {
+    public create(options: IndexDocumentParams<any>): Promise<any> {
         return this.promisify((callback: any) => {
-            let options: IndexDocumentParams<any> = {index: this.index, type: type, body: data};
-
-            if (parentId) {
-                options.parent = parentId;
-            }
-            options.refresh = 'true';
-
             this.client.index(options, callback);
         });
     }
 
     /**
-     * @param {string} type
-     * @param {string} id
-     * @param {string} parentId
+     * @param {GetParams} options
+     *
      * @returns {Promise}
      */
-    public get(type: string, id: string, parentId?: string): Promise<any> {
-        let options: GetParams = {index: this.index, type: type, id: id};
-
-        if (parentId) {
-            options.parent = parentId;
-        }
-
+    public get(options: GetParams): Promise<any> {
         return this.promisify((callback: any) => {
             this.client.get(options, callback);
         });
     }
 
     /**
-     * @param {string} type
-     * @param {string} id
-     * @param {Object} data
-     * @param {string} parentId
+     * @param {UpdateDocumentParams} options
+     *
      * @returns {Promise}
      */
-    public update(type: string, id: string, data: any, parentId?: string): Promise<any> {
-        let options: UpdateDocumentParams = {
-            index: this.index,
-            type: type,
-            id: id,
-            refresh: true,
-            body: {
-                doc: data
-            }
-        };
-
-        if (parentId) {
-            options.parent = parentId;
-        }
-
+    public update(options: UpdateDocumentParams): Promise<any> {
         return this.promisify((callback: any) => {
             this.client.update(options, callback);
         });
     }
 
     /**
-     * @param {string} type
-     * @param {string} id
-     * @param {string} parentId
+     * @param {DeleteDocumentParams} options
+     *
      * @returns {Promise}
      */
-    public remove(type: string, id: string, parentId?: string): Promise<any> {
-        let options: DeleteDocumentParams = {index: this.index, type: type, id: id};
-
-        if (parentId) {
-            options.parent = parentId;
-        }
-
+    public remove(options: DeleteDocumentParams): Promise<any> {
         return this.promisify((callback: any) => {
             this.client.delete(options, callback);
         });
     }
 
     /**
-     * @param {string} type
-     * @param {*} body
+     * @param {SearchParams} options
+     *
      * @returns {Promise}
      */
-    public search(type: string, body?: any): Promise<any> {
-        let options: SearchParams = {index: this.index, type: type};
-
-        if (body) {
-            options.body = body;
-        }
-
+    public search(options: SearchParams): Promise<any> {
         return this.promisify((callback: any) => {
             this.client.search(options, callback);
         });
     }
 
     /**
-     * @param {Object} options
+     * @param {any} options
      * @returns {Promise}
      */
-    public searchByOptions(options: any): Promise<any> {
-        options.index = this.index;
-
-        return this.promisify((callback: any) => {
-            this.client.search(options, callback);
-        });
-    }
-
-    /**
-     * @param {string} type
-     * @param {*} queryBody
-     * @returns {Promise}
-     */
-    public removeByQuery(type: string, queryBody: any): Promise<any> {
-        let options: any = {
-            index: this.index,
-            type: type,
-            conflicts: 'proceed'
-        };
-
-        if (queryBody) {
-            options.body = queryBody;
-        }
-
+    public removeByQuery(options: any): Promise<any> {
         return this.promisify((callback: any) => {
             this.client.deleteByQuery(options, callback);
         });
