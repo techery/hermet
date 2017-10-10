014c2561996c389219efb985b9ef380f8e1109db
Artem <kaminooni.box@gmail.com>
2017-10-10 13:22:04 +0300

    Change default port to 5025

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,30 @@
-import { config as load } from 'dotenv';
-import { env } from './helpers';
+import {config as load} from 'dotenv';
+import {env} from './helpers';
+
 load();
 
 const config: Config = Object.freeze({
     debug: env('DEBUG', false),
+
     app: {
-        port: env('HERMET_API_PORT', 5000),
+        port: env('HERMET_API_PORT', 5025),
         base_url: env('HERMET_API_BASE_URL', 'http://localhost'),
         session_header: env('HERMET_SESSION_HEADER', 'x-session-id'),
         default_stub_ttl: env('HERMET_DEFAULT_STUB_TTL', 300),
         default_service_ttl: env('HERMET_DEFAULT_SERVICE_TTL', 0),
         default_session_ttl: env('HERMET_DEFAULT_SESSION_TTL', 0)
     },
+
     log: {
         app: env('LOG_API_FILE_NAME', 'logs/api.log'),
         proxy: env('LOG_PROXY_FILE_NAME', 'logs/proxy.log'),
         level: env('LOG_LEVEL', 'warn')
     },
+
     proxy: {
-        port: env('HERMET_PROXY_PORT', 5050),
         timeout: env('PROXY_DEFAULT_TIMEOUT', 10000)
     },
+
     database: {
         file: 'database/db.json',
         autosave_interval: env('HERMET_DATABASE_AUTOSAVE_INTERVAL', 5000)
@@ -51,8 +55,7 @@ class Config {
     };
 
     public proxy: {
-        timeout: number,
-        port: number
+        timeout: number
     };
 
     public database: {
@@ -66,5 +69,5 @@ class Config {
     };
 }
 
-export { config, Config };
+export {config, Config};
 export default config;
