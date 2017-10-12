c241105414863de30c875ffbabdbb662c37fbd98
Artem <kaminooni.box@gmail.com>
2017-10-12 16:56:56 +0300

    Fix update action

--- src/models/Base.ts
+++ src/models/Base.ts
@@ -1,12 +1,17 @@
 import * as moment from 'moment';
 
 export class Base {
+    public $loki: string;
     public id: string;
     public createAt: string;
     public expireAt: string;
     public ttl: number;
 
     constructor(params: any = {}) {
+        if (params.$loki) {
+            this.$loki = params.$loki
+        }
+
         this.id = params.id || null;
         this.ttl = params.ttl || null;
         this.createAt = params.createAt || moment().format();
