e408441356cec9b61ed7a00bbd52f86bb4c640a6
Artem <kaminooni.box@gmail.com>
2017-10-12 17:21:25 +0300

    fix loki database issues

--- src/models/Base.ts
+++ src/models/Base.ts
@@ -1,20 +1,19 @@
 import * as moment from 'moment';
+import * as _ from 'lodash';
 
 export class Base {
-    public $loki: string;
     public id: string;
     public createAt: string;
     public expireAt: string;
     public ttl: number;
 
     constructor(params: any = {}) {
-        if (params.$loki) {
-            this.$loki = params.$loki;
-        }
-
         this.id = params.id || null;
         this.ttl = params.ttl || null;
         this.createAt = params.createAt || moment().format();
         this.expireAt = params.expireAt || null;
+
+        // Assign everything else
+        Object.assign(this, _.omit(params, _.keys(this)));
     }
 }
